308a2d0e7ea7c3542bb892ac08139eca68c0f429
Pratham Panchal <72151642+PrathamPanchalOfficial@users.noreply.github.com>
2022-07-02 11:05:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.querySelector('.spans').addEventListener("click",()=>{
+    document.querySelector('ul').classList.toggle('show');
+    document.querySelector('.one').classList.toggle('show1');
+    document.querySelector('.two').classList.toggle('show2');
+});
